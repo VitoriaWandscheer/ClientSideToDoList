ae66f46b2370d4ace616ddf2e44fa70b16703e6d
Vitória <vitoriawpereira2404@gmail.com>
2023-11-09 13:21:41 -0300

    ✨ feat: create user verification

--- js/User.js
+++ js/User.js
@@ -1,22 +1,35 @@
 class User {
-
+    // Atributo em lista contendo objetos json
     userAllowed = [
         {
+            name:"Vitória",
             email:"vitoria@gmail.com",
             password:"123"
         },
         {
+            name:"Yuri",
             email:"yuri@gmail.com",
             password:"321"
         },
         {
+            name:"Rafael",
             email:"rafael@gmail.com",
             password:"987"
         }
     ];
 
     login(email, password) {
-        alert("Método Login chamado com sucesso! =)")
+        this.userAllowed.map(function(item, index){
+            //console.log(item, index)
+
+            if(item.email == email){
+                if(item.password == password){
+                    alert("Olá "+item.name+", fico feliz em te ver aqui!")
+                } else {
+                    alert("Senha incorreta.")
+                }
+            }
+        })
     }
 
     logout() {
