7ae7a7d99cc5341c444b6039cb09452cd8385098
Vitória <vitoriawpereira2404@gmail.com>
2023-11-09 12:17:33 -0300

    ✨ feat: cretae list userAllowed

--- js/User.js
+++ js/User.js
@@ -1,5 +1,20 @@
 class User {
 
+    userAllowed = [
+        {
+            email:"vitoria@gmail.com",
+            password:"123"
+        },
+        {
+            email:"yuri@gmail.com",
+            password:"321"
+        },
+        {
+            email:"rafael@gmail.com",
+            password:"987"
+        }
+    ];
+
     login(email, password) {
         alert("Método Login chamado com sucesso! =)")
     }
